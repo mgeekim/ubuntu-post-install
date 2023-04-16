4b8caa388430b10b655b7c6b3304f441672335b8
mgeekim <64mingn@gmail.com>
2023-04-16 23:45:09 +0900

    Add default scripts

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+sudo -v
+
+BASE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+function import_dir {
+    local path=$1
+    for SCRIPT in $path/*;
+    do
+        . $SCRIPT
+    done
+}
+
+function install {
+    source /etc/lsb-release
+    source /etc/os-release
+
+    if [[ $EUID -eq 0 ]]; then
+        echo -e "${ERROR}This script must be ran as non-root. But needs sudoer for some commands.${END}"
+        exit 0
+    fi
+
+    echo -e "Your OS : \033[32m$DISTRIB_DESCRIPTION\033[0m"
+
+    if [[ ${DISTRIB_CODENAME} = "bionic" || ${DISTRIB_CODENAME} = "focal" ]]; then
+        :
+    else
+        echo -e "${ERROR}FAIL, CHECK your OS Version. 18.04, 20.04 is supported only${END}"
+        exit 0
+    fi
+
+    if grep -qEi "(Microsoft|microsoft|WSL)" /proc/version &> /dev/null; then
+        :
+    fi
+
+    # Import scripts
+    . ${BASE_PATH}/SETTING
+    import_dir ${BASE_PATH}/scripts
+
+    # Change ubuntu repository mirror
+    backup /etc/apt/sources.list
+    replace_string /etc/apt/sources.list "archive.ubuntu.com" ${UBUNTU_MIRROR}
+    replace_string /etc/apt/sources.list "security.ubuntu.com" ${UBUNTU_MIRROR}
+
+    # Install .gitconfig
+    install_config ~/.gitconfig ${BASE_PATH}/config/git/gitconfig
+
+    # Create NEW ssh key
+    if [ ! -f ~/.ssh/id_rsa ]; then
+        ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+    fi
+
+    sudo dpkg --add-architecture i386
+
+    # Apt update and upgrade
+    run sudo apt update
+    run sudo apt -y upgrade
+
+    # Install apt pacakges
+    install_apt "${BASE_PATH}/asset/packages.list"
+
+    # Install fonts
+    install_apt "${BASE_PATH}/asset/fonts.list"
+}
+
+install
